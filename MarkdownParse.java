b826bcaec91fcbe1e35baab4e0eceb6440a968f6
Gus-Mora <gmora@ucsd.edu>
2022-05-02 21:32:30 -0700

    fix for the second error test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,10 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+
+            if(markdown.indexOf("[") == markdown.indexOf("!") + 1){
+                break;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
